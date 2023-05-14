514b62d866c8f899f4cb21a6a333f1b176eca167
zigandreidelosr <127914385+zigandreidelosr@users.noreply.github.com>
2023-05-14 23:57:44 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appsdevproject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text.Equals("abcd")) && (textBox2.Text.Equals("abcd")))
+            {
+                Form f2 = new Form2();
+                f2.Show();
+
+                //MessageBox.Show("login Succesful");
+            }
+            else
+            {
+
+                MessageBox.Show("Username or Password is incorrect");
+            }
+        }
+    }
+}
